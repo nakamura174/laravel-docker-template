b9b83c9be6ea322e5af5c8d898318830dd2b95b6
nakamura174 <23liam.188@gmail.com>
2024-05-11 20:50:33 +0900

    add: Todo一覧表示機能の追加。

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Todo;
+
 class TodoController extends Controller
 {
     //
     public function index()
     {
+        $todo = new Todo();
+        $todoList = $todo->all();
+        dd($todoList);
+
         return view('todo.index');  
     }
 }
