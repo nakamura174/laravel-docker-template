c86b1f739d65b6c582a00af27ab0fc5046730533
nakamura174 <23liam.188@gmail.com>
2024-05-15 19:49:21 +0900

    refactor: Route・Viewのリファクタリング。

--- src/resources/views/todo/create.blade.php
+++ src/resources/views/todo/create.blade.php
@@ -13,22 +13,10 @@
     integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 </head>
 <body>
-  <div id="app">
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-      <div class="container">
-        <a class="navbar-brand" href="/todo">ToDo</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto"></ul>
-          <ul class="navbar-nav ml-auto"></ul>
-        </div>
-      </div>
-    </nav>
     <main class="py-4">
       <div class="container">
+      @extends('layouts.base')
+      @section('content')
         <div class="row justify-content-center">
           <div class="col-md-8">
             <div class="card">
@@ -54,5 +42,6 @@
       </div>
     </main>
   </div>
+  @endsection
 </body>
 </html>
