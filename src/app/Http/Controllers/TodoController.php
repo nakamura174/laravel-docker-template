5d76c7fe5553a709864a5f01a1e01667f22b5f47
nakamura174 <23liam.188@gmail.com>
2024-05-15 19:13:00 +0900

    add: 新規作成画面の表示機能の実装。

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -16,4 +16,9 @@ class TodoController extends Controller
 
         return view('todo.index', ['todoList' => $todoList]);  
     }
+
+    public function create()
+    {
+        return view('todo.create');
+    }
 }
