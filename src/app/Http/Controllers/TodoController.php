53fce01fc9794049ddb4a9b95194c7ec369006d4
nakamura174 <23liam.188@gmail.com>
2024-06-01 20:58:31 +0900

    refactor: モデルインスタンスを生成しControllerをリファクタリング。

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -9,10 +9,16 @@ use Illuminate\Http\Request;
 class TodoController extends Controller
 {
     //
+    private $todo;
+
+    public function __construct(Todo $todo)
+    {
+        $this->todo = $todo;
+    }
+
     public function index()
     {
-        $todo = new Todo();
-        $todoList = $todo->all();
+        $todoList = $this->todo->all();
 
         return view('todo.index', ['todoList' => $todoList]);  
     }
@@ -26,18 +32,15 @@ class TodoController extends Controller
     {
         $inputs = $request->all();
 
-        $todo = new Todo();
-        $todo->fill($inputs);
-        $todo->save();
+        $this->todo->fill($inputs);
+        $this->todo->save();
 
         return redirect()->route('todo.index');
     }
 
     public function show($id)
     {
-        $todo = new Todo();
-        $targetTodo = $todo->find($id);
-
-        return view('todo.show', ['todo' => $targetTodo]);
+        $todo = $this->todo->find($id);
+        return view('todo.show', ['todo' => $todo]);
     }
 }
